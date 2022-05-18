b28be6c90ef87a83cb7c6c2b0eace08feddd5b38
v-susu1 <v-susu@microsoft.com>
2022-05-18 10:40:50 +0800

    Update test1.ps1

--- test1.ps1
+++ test1.ps1
@@ -11,8 +11,8 @@ if($step -eq '1'){
 	Write-Host "##vso[task.setvariable variable=t2;isOutput=true]$b"
 	
 } else {
-	Write-Host "Pass parameter p1=$p1"
-	Write-Host "Pass parameter p2=$Env:t2"
+	Write-Host "Pass parameter p1 = $p1"
+	Write-Host "Pass parameter p2 = $Env:t2"
 	
 	$url = "http://www.google.com"
 	Write-Host ("##vso[task.setvariable variable=t3;]$url")
