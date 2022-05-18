53141f4c97c4887d542fe6ff4ff827d88919dd62
v-susu1 <v-susu@microsoft.com>
2022-05-18 09:28:17 +0800

    Update test1.ps1

--- test1.ps1
+++ test1.ps1
@@ -8,7 +8,7 @@ if($step -eq '1'){
 	$b = "http://www.baidu.com"
 	Write-Host "##vso[task.setvariable variable=t2;isOutput=true]$b"
 	
-} else if($step -eq '2') {
+} else {
 	Write-Host "Pass parameter p1=$p1"
 	Write-Host "Pass parameter p1=$Env:t2"
 }
